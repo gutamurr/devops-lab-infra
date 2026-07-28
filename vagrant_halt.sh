59024c03260f0114d0551ace96fc90b7baf85786
Gene <126711496+gutamurr@users.noreply.github.com>
2026-07-28 17:26:19 +0200

    Add script to halt Vagrant VMs

---
+++ vagrant_halt.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+BASE_DIR="${1:-.}"
+VAGRANT_DIR="$BASE_DIR/vagrant"
+
+echo "==> Start VMs on $VAGRANT_DIR"
+if [ -f "$VAGRANT_DIR/Vagrantfile" ]; then
+    (cd "$VAGRANT_DIR" && vagrant halt)
+else
+    echo "Error: Vagrantfile wasn't found in $VAGRANT_DIR"
+    exit 1
+fi
