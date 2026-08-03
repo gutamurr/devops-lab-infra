12a92e8960c48f235ed77161d6aab0b7d04232d2
Eugene <gutamurr@gmail.com>
2026-08-03 17:44:29 +0200

    Update ansible.sh to correct playbook names for app and Jenkins nodes

--- ansible.sh
+++ ansible.sh
@@ -6,7 +6,7 @@ ANSIBLE_DIR="$BASE_DIR/ansible"
 
 echo "==> Starting the ansible playbook in $ANSIBLE_DIR"
 if [ -d "$ANSIBLE_DIR" ]; then
-    (cd "$ANSIBLE_DIR" && ansible-playbook -i inventory playbooks/jenkins.yml playbooks/k3s.yml)
+    (cd "$ANSIBLE_DIR" && ansible-playbook -i inventory playbooks/app-node-playbook.yml playbooks/jenkins-node-playbook.yml)
 else
     echo "Error: Directory $ANSIBLE_DIR not found"
     exit 1
