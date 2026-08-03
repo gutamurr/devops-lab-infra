0c20f0aa92fc3197fe79045ed0d4001f9610f735
Eugene <gutamurr@gmail.com>
2026-08-03 17:44:15 +0200

    Update start.sh to include sleep before starting Ansible playbooks and correct playbook names

--- start.sh
+++ start.sh
@@ -13,9 +13,12 @@ else
     exit 1
 fi
 
+echo "==> Sleep for 15 seconds to allow VMs to boot up"
+sleep 15
+
 echo "==> Starting the ansible playbook in $ANSIBLE_DIR"
 if [ -d "$ANSIBLE_DIR" ]; then
-    (cd "$ANSIBLE_DIR" && ansible-playbook -i inventory playbooks/jenkins.yml playbooks/k3s.yml)
+    (cd "$ANSIBLE_DIR" && ansible-playbook -i inventory playbooks/app-node-playbook.yml playbooks/jenkins-node-playbook.yml)
 else
     echo "Error: Directory $ANSIBLE_DIR not found"
     exit 1
